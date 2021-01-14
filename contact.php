5a9c35623535af40f29e260f41c41befc30bb7b1
Katherine Hubacek <khubacek@bruinmail.slcc.edu>
2021-01-13 19:01:35 -0700

    changes references to php

--- contact.php
+++ contact.php
@@ -15,11 +15,11 @@
             <nav class="navbar navbar-expand-md">
                 <a href="#" class="navbar-brand"></a>
                    <ul class="navbar-nav">
-                    <li class="navbar-item"> <a href="./index.html" class="nav-link">Home</a>
-                    <li class="navbar-item"> <a href="./about.html" class="nav-link">About</a>
-                    <li class="navbar-item"> <a href="./portfolio.html" class="nav-link">Portfolio</a>
-                    <li class="navbar-item"> <a href="./contact.html" class="nav-link">Contact</a>
-                    <li class="navbar-item"> <a href="./test.html" class="nav-link">JavaScript</a>
+                    <li class="navbar-item"> <a href="./index.php" class="nav-link">Home</a>
+                    <li class="navbar-item"> <a href="./about.php" class="nav-link">About</a>
+                    <li class="navbar-item"> <a href="./portfolio.php" class="nav-link">Portfolio</a>
+                    <li class="navbar-item"> <a href="./contact.php" class="nav-link">Contact</a>
+                    <li class="navbar-item"> <a href="./test.php" class="nav-link">JavaScript</a>
                    <a href="./resume.docx"download Resume></a>
                    </ul>
                </nav>
